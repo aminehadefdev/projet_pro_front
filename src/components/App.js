d416e43db81cd3f348e46ff8a8ab6ed683ab62ff
aminehadef <amine.hadef.dev@gmail.com>
2020-11-14 23:09:15 +0100

    refont archi

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom"
-import './App.css';
+import '../css/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Home from './home/Home'
